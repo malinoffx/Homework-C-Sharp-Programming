8bf928110e6abc9d3c05a3d454cef8290ab9939c
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:09:32 +0200

    Chapter 3

---
+++ Глава 3 - Задача 12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Глава_3___Задача_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 5;
+            int p = 1;
+            int i = 1;
+            int mask = i << p;
+            if (mask == 1)
+            {
+                bool a = true;
+                Console.WriteLine(a);
+            }
+            else
+            {
+                bool b = false;
+                Console.WriteLine(b);
+            }
+
+            
+               
+            
+        }
+    }
+}
