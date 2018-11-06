f431e4c5fd9d0afc34e54dd2a198bc8b79e76afb
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:02:27 +0200

    Chapter 3

---
+++ Глава 3 - Задача 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Глава_3___Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15700;
+            int b = 157;
+            int c = b % 10;
+            Console.WriteLine();
+
+            if (c == 7)
+            {
+                Console.WriteLine("Tretata cifra e 7");
+            }
+            else
+            {
+                Console.WriteLine("Tretata cifra ne e 7");
+            }
+        }
+    }
+}
