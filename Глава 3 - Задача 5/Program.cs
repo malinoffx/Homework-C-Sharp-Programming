d6a0be7c69f26cb7ed861226b5cda6278e25c9be
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:04:09 +0200

    Chapter 3

---
+++ Глава 3 - Задача 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Глава_3___Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Insert parameter for a:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int b;
+            Console.WriteLine("Insert parameter for b:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int h;
+            Console.WriteLine("Insert parameter for h:");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your value is:");
+            Console.WriteLine((a + b) / 2 * h); 
+        }
+    }
+}
