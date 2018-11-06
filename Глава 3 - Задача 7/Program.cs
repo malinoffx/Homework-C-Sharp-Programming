4ec02c5ae05723ff8a52611627d282fa2ee2cf4a
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:05:42 +0200

    Chapter 3

---
+++ Глава 3 - Задача 7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Глава_3___Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your current weight:");
+            double weightEarth = double.Parse(Console.ReadLine());
+
+            double weightMoon = weightEarth * 0.17;
+            Console.WriteLine("Your weight {0} on the mood will be {1}", weightEarth, weightMoon);
+        }
+    }
+}
