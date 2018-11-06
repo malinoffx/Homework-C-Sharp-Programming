88f3a9eb9f36755d6b1a906e5b17c6767a3f835c
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 17:54:17 +0200

    Add files via upload

---
+++ Глава 3 - Задача 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Глава_3___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int mynumber = number % 2;
+
+            Console.WriteLine(new string('-', 40));
+
+            if (mynumber == 0)
+            {
+                Console.WriteLine("Chisloto e chetno");
+            }
+            else {
+                Console.WriteLine("Chisloto e ne chetno");
+            }
+        }
+    }
+}
