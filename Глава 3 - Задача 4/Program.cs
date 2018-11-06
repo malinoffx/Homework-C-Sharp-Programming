d7d59adae0668656f3ca04a04e83ef99c2e7d409
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:03:10 +0200

    Chapter 3

---
+++ Глава 3 - Задача 4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Глава_3___Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 30;
+            bool bit3 = (num & 8) != 0;
+            Console.WriteLine(bit3);
+        }
+    }
+}
