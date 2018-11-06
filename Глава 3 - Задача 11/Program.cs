239ac7f50af36cbfb09fc1cfbbe71ca39da91dc1
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:08:31 +0200

    Chapter 3

---
+++ Глава 3 - Задача 11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Глава_3___Задача_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 35;
+            int p = 6;
+            int i = 1;
+            int mask = 1 << p;
+            Console.WriteLine((n & mask) != 0 ? 1: 0);
+            Console.WriteLine();
+        }
+    }
+}
