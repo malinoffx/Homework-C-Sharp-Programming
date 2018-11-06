c5161a7ed18ea31ab455d7ac99b918d2dcf04b3d
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 17:59:21 +0200

    Chapter 3

---
+++ Глава 3 - Задача 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Глава_3___Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 35;
+
+            if (number % 35 == 0)
+            {
+                Console.WriteLine("Chisloto se deli na 7 i na 5 bez ostatuk ");
+            }
+            else
+            {
+                Console.WriteLine("Chisloto ne se deli na 7 i na 5 bez ostatuk");
+            }
+        
+        }
+    }
+}
