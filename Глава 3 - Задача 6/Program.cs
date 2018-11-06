f386eb9e17e8c1faf84c9a359b4ffc1240b83552
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:04:55 +0200

    Chapter 3

---
+++ Глава 3 - Задача 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Глава_3___Задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int a;
+            Console.WriteLine("Insert parameter for a:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int h;
+            Console.WriteLine("Insert parameter for h:");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value 1 is:");
+            Console.WriteLine(2 * a + 2 * h);
+            Console.WriteLine("Value 2 is:");
+            Console.WriteLine(a * h);
+        }
+    }
+}
