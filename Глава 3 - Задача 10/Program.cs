4739008bc5a322f55708c60d97aeba78994bb61e
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-06 18:07:48 +0200

    Chapter 3

---
+++ Глава 3 - Задача 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Глава_3___Задача_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter a 4 digit number: ");
+            num = int.Parse(Console.ReadLine());
+            int a = num % 10;
+            int b = (num / 10) % 10;
+            int c = (num / 100) % 10;
+            int d = (num / 1000) % 10;
+
+            Console.WriteLine("The Sum is {0}", a + b + c + d);
+            Console.WriteLine("Opposite direction is {0} {1} {2} {3}", a, b ,c , d);
+            Console.WriteLine("4th on 1st : {0} {1} {2} {3}", a, d, c, b);
+            Console.WriteLine("2nd and 3rd swap : {0} {1} {2} {3}", d, b, c, a);
+        }
+    }
+}
