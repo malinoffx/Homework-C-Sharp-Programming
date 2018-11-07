56fb536423cef69deac730c179a0c92f2c2e9fdc
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 19:02:34 +0200

    Chapter 4

---
+++ Глава 4 - Задача 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Глава_4___Задача_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.Write("Enter number count :");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter {0} number :", i + 1);
+                sum += int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Sum of all numbers is {0}", sum);
+        }
+    }
+}
