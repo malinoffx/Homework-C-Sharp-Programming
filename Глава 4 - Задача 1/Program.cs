4fbf3ffbb7da34ab7d62e26c727cf2da505110b6
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 18:53:09 +0200

    Chapter 4

---
+++ Глава 4 - Задача 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Глава_4___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", a + b + c);
+        }
+    }
+}
