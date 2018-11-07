0742d05a7f1237db0ca02d0497bc573c4e9d6d2d
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 19:05:31 +0200

    Chapter 4

---
+++ Глава 4 - Задача 13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Глава_4___Задача_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter last number :");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 1.0;
+
+            for (int i = 2; i < n; i++)
+            {
+                sum += (1.0 / i);
+            }
+            Console.WriteLine("{0:F3}", sum);
+
+
+
+
+        }
+    }
+}
