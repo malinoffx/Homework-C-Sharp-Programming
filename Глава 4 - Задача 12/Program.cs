560bce935729030c8ef154236217a7affa0c5f52
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 19:04:45 +0200

    Chapter 4

---
+++ Глава 4 - Задача 12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Глава_4___Задача_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f1 = 0;
+            int f2 = 1;
+            int sum = 1;
+            int count = 0;
+            Console.WriteLine(f1);
+            while (count < 100)
+            {
+                sum = f1 + f2;
+                f1 = f2;
+                f2 = sum;
+                Console.WriteLine(f2);
+                count++;
+            }
+        }
+    }
+}
