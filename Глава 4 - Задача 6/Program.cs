17d59ac4c5079d2b4a80943a74d4074e139fc97d
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 18:58:46 +0200

    Chapter 4

---
+++ Глава 4 - Задача 6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Глава_4___Задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Insert a :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert b :");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your result :");
+            Console.WriteLine("{0} > {1}", Math.Max(a, b), Math.Min(a, b));
+        }
+    }
+}
