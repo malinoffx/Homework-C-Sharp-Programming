1ab79b1099242ec6d035c81d201de09532e454b0
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 18:54:06 +0200

    Chapter 4

---
+++ Глава 4 - Задача 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Глава_4___Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for r: ");
+            int r = int.Parse(Console.ReadLine());
+            int d = 2 * r;
+            double pi = Math.PI;
+            Console.WriteLine("P is :");
+            Console.WriteLine(Math.Abs(d * pi));
+            Console.WriteLine("S is :");
+            Console.WriteLine(Math.Abs(pi *(r*r)));
+            
+            
+            
+
+        }
+    }
+}
