a33b07a57616e7b6c10ca28a89faa4e850977eb3
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 18:59:25 +0200

    Chapter 4

---
+++ Глава 4 - Задача 7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Глава_4___Задача_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            int count = 0;
+            int sum = 0;
+            bool boop = true;
+            Console.WriteLine("Insert your 5 numbers :");
+            while (boop == true)
+            {
+                try
+                {
+                    int a = int.Parse(Console.ReadLine());
+
+                    
+                    
+                        sum += a;
+                        count++;
+                    
+                    
+                    if (count == 5)
+                    {
+                        boop = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "Try again :");
+                    
+                }
+                
+            }
+            Console.WriteLine("Your result is :");
+            Console.WriteLine(sum);
+
+            
+            
+
+            
+        }
+    }
+}
