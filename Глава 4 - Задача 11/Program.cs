99703fc1a5ddceea74433bc47b647c0a84522c3d
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-07 19:03:25 +0200

    Chapter 4

---
+++ Глава 4 - Задача 11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Глава_4___Задача_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter number here :");
+            int j = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < j; i++)
+            {
+                Console.WriteLine(j);
+            }
+
+        }
+    }
+}
