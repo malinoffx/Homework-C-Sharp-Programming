a245afacbf51a3861dfc01bbdd073168e19a1f7f
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-10 17:50:01 +0200

    Chapter 5

---
+++ Глава 5 - Задача 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Глава_5___Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b :");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c :");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < 0 && b < 0 && c < 0)
+            {
+                Console.WriteLine(" - ");
+            }
+            else if (a >= 0 && b >= 0 && c >= 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (a < 0 && b < 0 && c >= 0)
+            {
+                Console.WriteLine(" + ");
+            }
+            else if (a < 0 && b >= 0 && c < 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (a >= 0 && b < 0 && c < 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (a < 0 && b >= 0 && c >= 0)
+            {
+                Console.WriteLine("-");
+            }
+            else if (a >= 0 && b < 0 && c >= 0)
+            {
+                Console.WriteLine("-");
+            }
+            else if (a >= 0 && b >= 0 && c < 0)
+            {
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
