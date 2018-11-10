2ef9c6828b95d8518b9e99c1a61621c3f2b8b802
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-10 17:48:54 +0200

    Chapter 5

---
+++ Глава 5 - Задача 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Глава_5___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                int a, b;
+                Console.WriteLine("Insert parameter for a :");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insert parameter for b :");
+                b = int.Parse(Console.ReadLine());
+                int c = a;
+
+                if (a > b)
+                {
+                    Console.WriteLine("{0} > {1}", a, b);
+                    Console.WriteLine("Your result = ");
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                }
+                else if (a == b)
+                {
+                    Console.WriteLine("Your result :");
+                    Console.WriteLine("They are equals");
+                }
+                else
+                {
+                    Console.WriteLine("Try Again");
+                }
+            }
+        }
+    }
+}
