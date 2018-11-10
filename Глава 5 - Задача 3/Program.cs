a50a14925da60254269eb4340cc54dff9920c4af
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-10 17:50:58 +0200

    Chapter 5

---
+++ Глава 5 - Задача 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Глава_5___Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b :");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c :");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("A is biggest!");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("B is biggest!");
+            }
+            else
+            {
+                Console.WriteLine("C is biggest!");
+            }
+        }
+    }
+}
