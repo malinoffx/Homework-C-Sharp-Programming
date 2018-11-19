0f155f90862361ef9cc84c376f7fb98f443bbfd0
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 19:13:27 +0200

    Chapter 6

---
+++ Глава 6 - Задача 15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_6___Задача_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+            int n = int.Parse(Console.ReadLine());
+            string toDecimal = Convert.ToString(Convert.ToInt32(n, 16), 10);
+            Console.WriteLine("Result is {0}", toDecimal);
+        }
+    }
+}
