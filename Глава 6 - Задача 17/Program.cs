226f9def4b667047371ef648e15496a8ec5cc510
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 19:18:01 +0200

    Chapter 6

---
+++ Глава 6 - Задача 17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Глава_6___Задача_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b) a %= b;
+                else b %= a;
+            }
+
+            if (a == 0) Console.WriteLine(b);
+            else Console.WriteLine(a);
+        }
+    }
+}
