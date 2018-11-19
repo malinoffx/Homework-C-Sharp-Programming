780c27ed4406c854b32077fe379d36f1819b7387
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 19:05:00 +0200

    Chapter 6

---
+++ Глава 6 - Задача 12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Глава_6___Задача_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input");
+            int value;
+            value = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine("Result: ");
+            Console.WriteLine(binary);
+        }
+    }
+}
