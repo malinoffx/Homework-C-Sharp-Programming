5529c80ae160ea02a9e1412828178d9dff1b5f1d
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 19:07:12 +0200

    Chapter 6

---
+++ Глава 6 - Задача 13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_6___Задача_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            int n = int.Parse(Console.ReadLine());
+            string toDouble = Convert.ToString(Convert.ToInt32(n, 2), 10);
+            Console.WriteLine("Result is {0}", toDouble);
+        }
+    }
+}
