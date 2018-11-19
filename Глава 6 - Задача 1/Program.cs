7a1b8cb0686af03e37db71c23b5d1c6dee8e7c44
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 18:46:28 +0200

    Chapter 6

---
+++ Глава 6 - Задача 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Глава_6___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your Result is :");
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
