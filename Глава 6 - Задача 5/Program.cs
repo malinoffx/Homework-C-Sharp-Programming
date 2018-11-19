f18c9b615b174314a8994fbd738d0d2671885246
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 18:51:28 +0200

    Chapter 6

---
+++ Глава 6 - Задача 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Глава_6___Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int a, b, s;
+            a = 0;
+            b = 1;
+            s = 0;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                s = a + b;
+                Console.WriteLine(s);
+                a = b;
+                b = s;
+            }
+        }
+    }
+}
