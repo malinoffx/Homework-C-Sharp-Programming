afe6a3f85e785643276a56d08d78a043f4c60531
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-19 18:47:40 +0200

    Chapter 6

---
+++ Глава 6 - Задача 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Глава_6___Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter:");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result:");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % (3*7) == 0)
+                {
+                    Console.WriteLine(" ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
