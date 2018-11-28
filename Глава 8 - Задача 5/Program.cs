377e713ac167465d466e7dd634888a2c28dca1de
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 13:08:06 +0200

    Chapter 8

---
+++ Глава 8 - Задача 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_8___Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            string binary = Console.ReadLine();
+            Console.WriteLine("{0} to decimal is {1}.",
+                binary, Convert.ToInt64(binary, 2));
+        }
+    }
+}
