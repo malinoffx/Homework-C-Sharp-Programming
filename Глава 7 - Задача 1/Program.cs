281e721b0679eae92ccc5f8c5e6568988ea4ed71
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 12:28:54 +0200

    Chapter 7

---
+++ Глава 7 - Задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Глава_7___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
