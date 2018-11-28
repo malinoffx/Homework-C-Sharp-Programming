9251aecd6ce011f80999e55bc03ffbcb2cfb4a0a
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 13:07:19 +0200

    Chapter 8

---
+++ Глава 8 - Задача 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_8___Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+            Console.WriteLine("Резултата е {0}", binary);
+        }
+    }
+}
