f41dbe50e5e2eebdb36426c10be60739510b1e43
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 13:10:44 +0200

    Chapter 8

---
+++ Глава 8 - Задача 9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_8___Задача_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            string binary = Console.ReadLine();
+            Console.WriteLine("{0} to hexadecimal is {1}.",
+                binary, Convert.ToInt32(binary, 2).ToString("X"));
+        }
+    }
+}
