d6395eeae1be1c4946bbc1a4332d6607790464ef
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 12:45:23 +0200

    Chapter 7

---
+++ Глава 7 - Задача 19/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Глава_7___Задача_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int n in nums)
+            {
+                if (!printZero && n == 0) continue;
+                Console.Write("{0}\t", n);
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            // 1 is not a prime number
+            for (int i = 2; i <= 10000000; ++i) nums.Add(i);
+
+            int p = 2;
+            int indexAt = 0;
+            bool limitReached = false;
+
+            while (!limitReached)
+            {
+                for (int i = 0; i < nums.Count; ++i)
+                {
+                    int v = nums[i];
+                    if (v % p == 0 && v != p) nums[i] = 0;
+                }
+                do
+                {
+                    p = nums[++indexAt];
+                    if (indexAt >= nums.Count - 1)
+                    {
+                        limitReached = true;
+                        break;
+                    }
+                } while (p == 0);
+            }
+
+            PrintList(nums, false);
+        }
+    }
+}
