002e1524e9dac8cfc70c941a829cfae4708bab99
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-11-28 13:09:21 +0200

    Chapter 8

---
+++ Глава 8 - Задача 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Глава_8___Задача_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+            int deci = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("{0} to hexadecimal is {1}.",
+                deci, deci.ToString("X"));
+        }
+    }
+}
