9adeaf14aa5a6d1abc9fdb446158b39e7cf5de49
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 23:22:46 +0200

    Курсова Работа - Крушков

---
+++ Задача 413/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Задача_413
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] Masive = new double[5];
+            RealNumber(Masive);
+            AverageThree(Masive);
+
+        }
+        static void RealNumber(double[]Double)
+        {
+            for (int i = 0; i < Double.Length; i++)
+            {
+                Console.WriteLine("Enter number :");
+                Double[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+        static void AverageThree(double[]Three)
+        {
+            int count = 0;
+            double sum = 0;
+
+            for (int i = 0; i < Three.Length; i++)
+            {
+                
+
+                if (Three[i] % 3 == 0)
+                {
+                    sum += Three[i];
+                    count++;
+                }
+
+                
+
+                    
+            }
+            Console.WriteLine("Your result is: ");
+            Console.WriteLine(sum / count);
+        }
+    }
+}
