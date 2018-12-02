d457808f3dec29e0f98a229a420a76772ea9a314
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 17:52:12 +0200

    Chapter 9

---
+++ Глава 9 - Задача 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Глава_9___Задача_3
+{
+    class Program
+    {
+         static void InputNum(string number)
+        {
+            switch (number[number.Length - 1])
+            {
+                case '1': Console.WriteLine("One"); break;
+                case '2': Console.WriteLine("Two"); break;
+                case '3': Console.WriteLine("Three"); break;
+                case '4': Console.WriteLine("Four"); break;
+                case '5': Console.WriteLine("Five"); break;
+                case '6': Console.WriteLine("Six"); break;
+                case '7': Console.WriteLine("Seven"); break;
+                case '8': Console.WriteLine("Eight"); break;
+                case '9': Console.WriteLine("Nine"); break;
+                case '0': Console.WriteLine("Zero"); break;
+                default: Console.WriteLine("Wrong input."); break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter mumber: ");
+            string number = Console.ReadLine();
+
+            InputNum(number);
+        }
+    }
+}
