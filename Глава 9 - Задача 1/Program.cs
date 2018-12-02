10deb7404c5f4fab0752de4bb2df4f1342283f1e
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 17:49:35 +0200

    Chapter 9

---
+++ Глава 9 - Задача 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Глава_9___Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+
+
+            static void ReturnName(string name)
+            {
+                Console.WriteLine("Hello {0}", name);
+            }
+        }
+
+            
+        
+    }
+}
