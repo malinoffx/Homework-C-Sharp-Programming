e6846f3ab1cb932e8cb4ad109350e7b2c80b35d6
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 18:24:01 +0200

    Chapter 10

---
+++ Глава 10 - Задача 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Глава_10___Задача_1
+{
+    class Program
+    {
+        static void Recur(int[] arr, int index)
+        {
+            if (index >= arr.Length)
+            {
+                foreach (int element in arr) Console.Write("{0} ", element);
+                Console.WriteLine();
+            }
+            else
+                for (int i = 1; i <= arr.Length; i++)
+                {
+                    arr[index] = i;
+                    Recur(arr, index + 1);
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Recur(arr, 0);
+        }
+    }
+}
