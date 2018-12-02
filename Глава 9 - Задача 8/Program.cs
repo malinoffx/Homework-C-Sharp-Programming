8e8e5cdb90bd74259cc9fc59c60701292bf32129
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 17:56:33 +0200

    Chapter 9

---
+++ Глава 9 - Задача 8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Глава_9___Задача_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int[] Arr1 = new int[10000];
+            int[] Arr2 = new int[10000];
+
+            Console.Write("Въведете първото число: ");
+            String s1 = Console.ReadLine();
+            length = s1.Length;
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Arr1[i] = Convert.ToInt32(s1.Substring(s1.Length - 1 - i, 1));
+            }
+
+            Console.Write("Въведете второто число: ");
+            String s2 = Console.ReadLine();
+
+            if (s2.Length > length)
+            {
+                length = s2.Length;
+            }
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                Arr2[i] = Convert.ToInt32(s2.Substring(s2.Length - 1 - i, 1));
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                Arr1[i] += Arr2[i];
+
+                if (Arr1[i] >= 10)
+                {
+                    Arr1[i] -= 10;
+                    Arr1[i + 1]++;
+                }
+            }
+
+            if (Arr1[length] == 1)
+            {
+                length++;
+            }
+
+            Console.WriteLine();
+            Console.Write("Резултат: ");
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(Arr1[length - 1 - i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
