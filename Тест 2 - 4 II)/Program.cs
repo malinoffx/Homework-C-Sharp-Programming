b02c6d9615122b36e99109cf8d40e06c003cd40e
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 23:23:22 +0200

    Курсова Работа - Крушков

---
+++ Тест 2 - 4 II)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Тест_2___4_II_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            
+            Console.WriteLine("Enter number between 1-7:");
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+                if (n < 0)
+                {
+                    Console.WriteLine("Wrong number. Try Again!");
+                }
+                else if (n > 9)
+                {
+                    Console.WriteLine("Wrong number.Try Again!");
+                }
+
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Понеделник"); break;
+                    case 2:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Вторник"); break;
+                    case 3:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Сряда"); break;
+                    case 4:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Четвъртак"); break;
+                    case 5:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Петък"); break;
+                    case 6:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Събота"); break;
+                    case 7:
+                        Console.WriteLine("The day of the week is:");
+                        Console.WriteLine("Неделя"); break;
+
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+}
