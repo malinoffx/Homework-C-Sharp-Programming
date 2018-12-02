b2284bd013eac70a5277d1d4876d0d148a8ead04
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 23:21:47 +0200

    Курсова Работа - Крушков

---
+++ Задача 367/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Курсова_Работа_2018___CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+            
+            Console.WriteLine("Enter number between 0-9:");
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+
+                if (n < 0)
+                {
+                    Console.WriteLine("Wrong number. Try Again!");
+                }
+                else if(n > 9)
+                {
+                    Console.WriteLine("Wrong number.Try Again!");
+                }
+                
+
+                switch (n)
+                {
+                    case 0:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Нула"); break;
+
+                    case 1:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Едно"); break;
+                    case 2:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Две"); break;
+                    case 3:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Три"); break;
+                    case 4:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Четири"); break;
+                    case 5:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Пет"); break;
+                    case 6:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Шест"); break;
+                    case 7:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Седем"); break;
+                    case 8:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Осем"); break;
+                    case 9:
+                        Console.WriteLine("Word: ");
+                        Console.WriteLine("Девет"); break;
+
+                    default:
+                        break;
+
+                        
+                }
+            }
+
+        }
+    }
+}
