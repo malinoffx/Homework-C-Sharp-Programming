b7b5908c326c7a375c1412d863d85bc27b4677c9
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 18:26:48 +0200

    Chapter 10

---
+++ Глава 10 - Задача 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Глава_10___Задача_3
+{
+    class Program
+    {
+        static void GetCombo(int[] arr, int index, int start, int end)
+        {
+            // генерирует число от  n до k .Выводит как множество( n - с какого по какое число, k - количество чисел в множестве)
+            if (index >= arr.Length)
+            {
+                Console.Write("(");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write("), ");
+            }
+            else
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    GetCombo(arr, index + 1, i, end);
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Въведете K: ");
+            int k = Int32.Parse(Console.ReadLine());
+            int[] arr = new int[k];
+            GetCombo(arr, 0, 1, n);
+        }
+    }
+}
