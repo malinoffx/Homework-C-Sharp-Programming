e9ecef98bcc64cd2b50f2038190d20588bfcd05f
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 17:54:51 +0200

    Chapter 9

---
+++ Глава 9 - Задача 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Глава_9___Задача_7
+{
+    class Program
+    {
+        public static string Circl(string number)
+        {
+            char[] charArr = number.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете число: ");
+            string number = Console.ReadLine();
+
+            Console.WriteLine(Circl(number));
+        }
+    }
+}
