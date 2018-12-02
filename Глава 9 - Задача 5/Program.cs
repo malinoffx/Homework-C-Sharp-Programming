7082ff60aef9b7fa79c62130836ca3283e5b6af7
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 17:53:47 +0200

    Chapter 9

---
+++ Глава 9 - Задача 5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Глава_9___Задача_5
+{
+    class Program
+    {
+        static void CompareNumb(int pos, int[] arr)
+        {
+            if (pos == 0)
+            {
+                if (arr[0] < arr[1]) Console.WriteLine("{0} е по-малко от дясното число.", arr[0]);
+                else if (arr[0] > arr[1]) Console.WriteLine("{0} е по-голямо от дясното число.", arr[0]);
+                else Console.WriteLine("{0} е равно на дясното число.", arr[0]);
+            }
+            else if (pos == arr.Length - 1)
+            {
+                if (arr[arr.Length - 1] < arr[arr.Length - 2]) Console.WriteLine("{0} е по-малко от лявото число.", arr[pos]);
+                else if (arr[arr.Length - 1] > arr[arr.Length - 2]) Console.WriteLine("{0} е по-голямо от лявото число.", arr[pos]);
+                else Console.WriteLine("{0} е равно на лявото число.", arr[pos]);
+            }
+            else
+            {
+                if (arr[pos] < arr[pos - 1])
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0} е по-малко от съседните числа.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0} е по-малък от лявото и равен на дясното число.", arr[pos]);
+                    else Console.WriteLine("{0} е по-малък от лявото и по-голям от дясното число", arr[pos]);
+                }
+                else if (arr[pos] == arr[pos - 1])
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0} е равно на лявато и по-малко от дясното число.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0} е равно на съседнити числа.", arr[pos]);
+                    else Console.WriteLine("{0} е равно на лявато и по-голямо от дясното число..", arr[pos]);
+                }
+                else
+                {
+                    if (arr[pos] < arr[pos + 1]) Console.WriteLine("{0}  е по-голямо от лявато и по-малко от дясното число.", arr[pos]);
+                    else if (arr[pos] == arr[pos + 1]) Console.WriteLine("{0}  е по-голямо от лявато и равно на дясното число.", arr[pos]);
+                    else Console.WriteLine("{0} е по-голямо от съседните числа.", arr[pos]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете дължината на масива: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Въведете {0} eлемент: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Въведите позицията в масива: "); 
+            int pos = Int32.Parse(Console.ReadLine());
+
+            CompareNumb(pos, arr);
+        }
+}
