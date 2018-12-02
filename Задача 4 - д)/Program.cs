50d79de8854ce68fa0e8f6da044dc0831f245a1b
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 23:19:03 +0200

    Курсова Работа - Крушков

---
+++ Задача 4 - д)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Задача_4___д_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("Enter number for x: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for y: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for z: ");
+            z = int.Parse(Console.ReadLine());
+
+            double c = x + y;
+
+            double d = z + (z / (x + (z / (y - x))));
+            Console.WriteLine("Your answer for (x+y) / z + (z / (x + (z / (y - x)))) is: ");
+            Console.WriteLine(c/d);
+
+        }
+    }
+}
