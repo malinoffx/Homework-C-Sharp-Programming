83f25538e0b2560e21187eb7240a5eb50ad637ee
Билял Малинов <44162145+malinoffx@users.noreply.github.com>
2018-12-02 18:26:11 +0200

    Chapter 10

---
+++ Глава 10 - Задача 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Глава_10___Задача_2
+{
+    class Program
+    {
+        static void GetCombo(int[] arr, int index, int start, int end)
+        {
+
+            if (index >= arr.Length)
+            {
+                Console.Write("(");
+                for (int i = 0; i < arr.Length; i++)
+                    if (i < arr.Length - 1) Console.Write("{0} ", arr[i]);
+                    else Console.Write(arr[i]);
+                Console.Write(")   ");
+            }
+            else
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    GetCombo(arr, index + 1, i, end);
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете N: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int k = n;
+            int[] arr = new int[k];
+            GetCombo(arr, 0, 1, n);
+        }
+    }
+}
